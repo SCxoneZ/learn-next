22ad7cb0f16b05fcc91972100bd3730727c1b4ba
SCxoneZ <stevenniuschandra@gmail.com>
2022-04-17 16:17:17 +0700

    getStaticProps

---
+++ pages/users/index.js
@@ -0,0 +1,29 @@
+export default function Users({ users }){
+  return (
+  
+  <>
+    <h1>Users List</h1>
+    {users.map(user => {
+      return (
+      
+      <ul>
+        <li>{user.name}</li>
+        <li>{user.email}</li>
+        <li>{user.phone}</li>
+      </ul>
+      );
+    })}
+  </>
+  
+  );
+}
+
+export async function getStaticProps(){
+  const request = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await request.json();
+  return {
+    props: {
+      users: data
+    }
+  };
+}
